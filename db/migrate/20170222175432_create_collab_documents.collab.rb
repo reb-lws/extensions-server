eede0ef0c902c1c819363897a629baf58b795a5c
Mo Bitar <me@bitar.io>
2017-02-22 20:00:09 -0600

    collab ext

--- db/migrate/20170222175432_create_collab_documents.collab.rb
+++ db/migrate/20170222175432_create_collab_documents.collab.rb
@@ -3,6 +3,8 @@ class CreateCollabDocuments < ActiveRecord::Migration[5.0]
   def change
     create_table :collab_documents do |t|
       t.text :content
+      t.string :iv
+      t.string :auth
       t.string :uuid
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@ ActiveRecord::Schema.define(version: 20170222175432) do
 
   create_table "collab_documents", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text     "content",    limit: 65535
+    t.string   "iv"
+    t.string   "auth"
     t.string   "uuid"
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
