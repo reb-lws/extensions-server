7d4c66fc35492330078380a096631f49caf4a826
Mo Bitar <me@bitar.io>
2017-03-13 12:30:52 -0500

    simple markdown tab support

--- app/assets/javascripts/editors/simple_markdown.js
+++ app/assets/javascripts/editors/simple_markdown.js
@@ -15,6 +15,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
     preview.innerHTML = md.render(window.noteText);
   }, false);
 
+  editor.addEventListener('keydown', function(event){
+    if (!event.shiftKey && event.which == 9) {
+      event.preventDefault();
+      var start = this.selectionStart;
+      var end = this.selectionEnd;
+      var spaces = "    ";
+
+      // Insert 4 spaces
+      this.value = this.value.substring(0, start) + spaces + this.value.substring(end);
+
+      // Place cursor 4 spaces away from where
+      // the tab key was pressed
+      this.selectionStart = this.selectionEnd = start + 4;
+    }
+  });
+
   if(window.parent != window) {
     window.parent.postMessage({status: "ready"}, '*');
   }
